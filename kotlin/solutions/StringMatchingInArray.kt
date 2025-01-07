bfb893d143420518b7cdbe1cd9f213c24e752fc4
Aleksandr Pavlyuk <pavlyukas@bcs.ru>
2025-01-08 02:15:07 +0700

    08.01.25

--- kotlin/solutions/StringMatchingInArray.kt
+++ kotlin/solutions/StringMatchingInArray.kt
@@ -1,16 +1,16 @@
 package solutions
 
-	fun stringMatching(words: Array<String>): List<String> {
-		val res = mutableListOf<String>()
+fun stringMatching(words: Array<String>): List<String> {
+	val res = mutableListOf<String>()
 
-		for (word in words) {
-			if (words.filter { it != word }.any { word in it }) {
-				res.add(word)
-			}
+	for (word in words) {
+		if (words.filter { it != word }.any { word in it }) {
+			res.add(word)
 		}
-
-		return res
 	}
+
+	return res
+}
 
 fun main() {
 	println(stringMatching(arrayOf("mass","as","hero","superhero")))

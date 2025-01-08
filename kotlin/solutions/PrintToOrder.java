41b55dd7facfdcabb0f614a9ff20f8ce6f19f306
Aleksandr Pavlyuk <pavlyukas@bcs.ru>
2025-01-08 23:23:53 +0700

    08.01.25

---
+++ kotlin/solutions/PrintToOrder.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.Semaphore;
+
+public class PrintToOrder {
+    private final Semaphore sem1;
+    private final Semaphore sem2;
+    
+    public PrintToOrder() {
+        sem1 = new Semaphore(0);
+        sem2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        sem1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        sem1.acquire();
+        printSecond.run();
+        sem2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        sem2.acquire();
+        printThird.run();
+    }
+}

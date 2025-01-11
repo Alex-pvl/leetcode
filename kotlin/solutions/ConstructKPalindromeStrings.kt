300bf9a5036318eda22eb32c74fbdae3969f542d
Aleksandr Pavlyuk <pavlyukas@bcs.ru>
2025-01-11 15:33:00 +0700

    11.01.25

--- kotlin/solutions/ConstructKPalindromeStrings.kt
+++ kotlin/solutions/ConstructKPalindromeStrings.kt
@@ -14,6 +14,15 @@ fun canConstruct(s: String, k: Int): Boolean {
 
 fun canConstruct2(s: String, k: Int) = s.groupingBy { it }.eachCount().count { it.value % 2 != 0 } <= k && k <= s.length
 
+fun canConstruct3(s: String, k: Int): Boolean {
+    var mask = 0
+    for (c in s) {
+        mask = mask xor (1 shl (c - 'a'))
+    }
+    val oddCount = mask.countOneBits()
+    return oddCount <= k && k <= s.length
+}
+
 fun main() {
     println(canConstruct("annabelle", 2))
 }
